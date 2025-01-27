5c9ef52726fb60d9350fa7f2bc477b24c05ade95
Kevin Heng <147899788+Kevin-Heng@users.noreply.github.com>
2025-01-27 10:31:56 +0800

    scripts for traffic system

---
+++ Assets/Scripts/PedestriannTrafficLight.cs
@@ -0,0 +1,58 @@
+/*
+    Author: Kevin Heng
+    Date: 27/1/2025
+    Description: The Pedestrian class is used to handle the traffic light system for player to cross the road
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianTrafficLight : MonoBehaviour
+{
+    /// <summary>
+    /// Boolean to set to true when player presses traffic light button
+    /// </summary>
+    bool buttonPressed;
+    /// <summary>
+    /// Boolean to set to true if player can cross the road
+    /// </summary>
+    public bool canCross;
+
+
+    public void ChangeLight()
+    {
+        if (buttonPressed)
+        {
+            //wait a few seconds
+            //if
+            //traffic light change from green to amber to red
+            if (canCross)
+            {
+                //change to green man and show timer text
+                //when timer = 0
+                canCross = false;
+                //change to red man and traffic light change to green
+            }
+
+        }
+        else
+        {
+            //wait a few seconds
+            //then
+            //traffic light change from red to green
+            //then
+            //change to red man
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour
+{
+    public PedestrianTrafficLight pedestrianTrafficLight;
+
+    public void Move()
+    {
+        if (!pedestrianTrafficLight.canCross)
+        {
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
